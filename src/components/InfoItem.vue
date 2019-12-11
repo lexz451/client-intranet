e24407cfbae366614a3318dc6c37d03c395de854
lexz451 <lexz.zaldivar@gmail.com>
2019-12-11 00:23:01 -0500

    Fix info items

--- src/components/InfoItem.vue
+++ src/components/InfoItem.vue
@@ -2,14 +2,19 @@
 	<div class="info-item">
 		<div class="row">
 			<div class="col">
-				<img v-lazy="item.image" class="img" alt="">
+				<!--<img v-lazy="item.image" class="img" alt="">-->
+				<img src="~/assets/images/hero.jpg" class="img" alt="">
 			</div>
 			<div class="col">
-				<span class="title">{{ item.title }}</span>
+				<h6 class="title">{{ item.title }} Internet en Cuba</h6>
+				<small>Last update: </small>
 			</div>
 		</div>
-		<div class="row action">
-			<b-button size="sm" class="" ><i class="fa fa-file"></i> View</b-button>
+		<div class="row actions">
+			<div class="col">
+				<button class="btn btn-secondary btn-sm" size="sm"><i class="fa fa-file"></i> View</button>
+				<a class="btn btn-primary btn-sm" role="button" href="#" size="sm"><i class="fa fa-file"></i> Download</a>
+			</div>
 		</div>
 	</div>
 </template>
@@ -28,16 +33,25 @@ export default {
 
 <style lang="scss">
 .info-item {
-	margin: 0 8px;
-	width: 260px;
+	margin: 4px 8px;
+	width: 300px;
+	height: 150px;
 	background: #fff;
 	padding: 12px 20px;
 	border-radius: 5px;
 	box-shadow: var(--bg-shadow-lg);
+	position: relative;
 	img {
 		width: 100%;
+		height: 100%;
 		object-fit: cover;
 		object-position: center;
+	}
+	.actions {
+		position: absolute;
+		bottom: 20px;
+		left: 20px;
+		
 	}
 }
 </style>
