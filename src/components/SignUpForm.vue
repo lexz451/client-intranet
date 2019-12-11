454e3a601c8a0e85467fad093f5eec2c856bb90e
lexz451 <lexz.zaldivar@gmail.com>
2019-12-11 01:31:44 -0500

    Fix

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -20,7 +20,7 @@
 		<form-input v-model="credentials.country" placeholder="Country" addonLeftIcon="fa fa-globe"></form-input>
 		<form-input v-model="credentials.email" placeholder="Email" addonLeftIcon="fa fa-envelope"></form-input>
 		<form-input v-model="credentials.password" placeholder="Password" addonLeftIcon="fa fa-lock"></form-input>
-		<form-input v-model="credentials.passwordRepeat" placeholder="Repeat Password" addonLeftIcon="fa fa-lock"></form-input>
+		<form-input v-model="passwordRepeat" placeholder="Repeat Password" addonLeftIcon="fa fa-lock"></form-input>
 		<b-button @click="onSubmit" type="primary">Sign Up</b-button>
 		<template #footer>
 			<span>Have an account? <router-link :to="{name: 'sign-in'}">Sign In</router-link></span>
